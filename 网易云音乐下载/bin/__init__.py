ab8f50cda21ba258a473929a02a72668ed00d03b
allyxmiko <allyxmiko@163.com>
2020-07-04 11:25:10 +0800

    del text files

--- 网易云音乐下载/bin/__init__.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python3
-# -*- encoding: utf-8 -*-
-# @File    : __init__.py.py
-# @Project : PythonTools
-# @Time    : 2020/7/4 10:57
-# @Author  : Allyx Miko
-
-# Here put the import lib
--- 网易云音乐下载/网易云音乐下载.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python3
-# -*- encoding: utf-8 -*-
-# @File    : 网易云音乐下载.py
-# @Project : PythonTools
-# @Time    : 2020/7/4 10:57
-# @Author  : Allyx Miko
-
-# Here put the import lib
-测试
