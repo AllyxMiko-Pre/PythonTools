353ef4dbd1625a078c2f9bf3ca1853040cee9b46
allyxmiko <allyxmiko@163.com>
2020-07-04 10:49:05 +0800

    update test

---
+++ 网易云音乐下载/测试.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env python3
+# -*- encoding: utf-8 -*-
+# @File    : 测试.py
+# @Project : PythonTools
+# @Time    : 2020/7/4 10:46
+# @Author  : Allyx Miko
+
+# Here put the import lib
+print("这是一个测试项目")
